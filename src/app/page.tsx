710f8ea14942aab51c5d02219ba18fd21a479cd2
JDaniloC <jdsc@cin.ufpe.br>
2024-11-25 21:53:15 -0300

    Feature: Project logic implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,83 @@
+"use client";
+
+import { useState } from 'react';
 import Image from "next/image";
+import Link from "next/link";
+
+import stories from "./stories.json";
+
+type StoryMetadataType = {
+  identifier: string;
+  title: string;
+  image: string;
+  classification: string;
+}
+
+const typedStories: StoryMetadataType[] = stories;
 
 export default function Home() {
+  const [audioEnabled, setAudioEnabled] = useState<boolean>(false);
+  const [imageEnabled, setImageEnabled] = useState<boolean>(false);
+
+  function toggleAudio() {
+    console.log('toggleAudio', audioEnabled);
+    setAudioEnabled(prev => !prev);
+  }
+
+  function toggleImage() {
+    setImageEnabled(prev => !prev);
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        {typedStories.map((story) => (
+          <Link
+            key={story.identifier}
+            href={{
+              pathname: `/${story.identifier}`,
+              query: { audio: audioEnabled, image: imageEnabled },
+            }}
+            className="text-primary hover:underline"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={story.image}
+              alt="Story image"
+              width={200}
+              height={200}
+              priority
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {story.title}
+            <span>
+              {story.classification}
+            </span>
+          </Link>
+        ))}
+
+        <div className="flex gap-4">
+          <label htmlFor="audio">Ativar áudio</label>
+          <input
+            type="checkbox"
+            name="audio"
+            id="audio"
+            value={audioEnabled}
+            onChange={toggleAudio}
+            className="hidden"
+          />
+          {audioEnabled}
         </div>
+        <div className="flex gap-4">
+          <label htmlFor="image">Ativar imagem</label>
+          <input
+            type="checkbox"
+            name="image"
+            id="image"
+            value={imageEnabled}
+            onChange={toggleImage}
+            className="hidden"
+          />
+        </div>
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -63,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Termos
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,22 +108,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Saiba mais
         </a>
       </footer>
     </div>
